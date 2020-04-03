f4a2f2f7015cbce138489bdc3d88e5f0144f95a5
Lucas P <lucas.pavloff@gmail.com>
2020-04-02 20:03:34 -0400

    Initial Commit

---
+++ Two Button Problem/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Two Button Problem
+//
+//  Created by Evgueni Pavlov on 4/2/20.
+//  Copyright © 2020 Evgueni Pavlov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
